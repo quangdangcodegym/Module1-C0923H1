e94692d7e0ae001a9f51670788fc89b665689022
quangdangcodegym <quang.dang@codegym.vn>
2023-10-11 12:24:36 +0700

    javascript

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function handleChangeColor() {
+    /** getElementById lấy thẻ theo ID */
+    // document.getElementById("txtHello").style.color = "red";
+
+    // document.querySelector("#txtHello").style.color = "blue";
+    // document.querySelectorAll(".txtTitle")[0].style.color = "red";
+    // document.querySelectorAll(".txtTitle")[1].style.color = "red";
+    // document.querySelectorAll(".txtTitle")[2].style.color = "red";
+    // document.querySelectorAll(".txtTitle")[3].style.color = "red";
+
+    document.querySelector(".txtTitle").style.color = "blue";
+}
